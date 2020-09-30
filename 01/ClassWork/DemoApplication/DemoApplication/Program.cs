438c76e6f7fbaaac8d1f01dc5b8f479f9180ffb7
Mbogdanov1984Ru <49115879+Mbogdanov1984Ru@users.noreply.github.com>
2020-09-30 22:39:06 +0300

    Class work of the 1st lesson added

---
+++ 01/ClassWork/DemoApplication/DemoApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace DemoApplication
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine("Введите имя");
+			string name = Console.ReadLine();
+			Thread.Sleep(5000);
+			Console.WriteLine($"Hello, {name}!");
+			Console.ReadKey();
+		}
+	}
+}
