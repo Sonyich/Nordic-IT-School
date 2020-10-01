01dfef20627b1496d6823951466d8a19dda27629
Sofya Borovikova <sonicbor@yandex.ru>
2020-10-01 14:42:49 +0300

    HomeWork of the 1st lesson added
    	new file:   01/HomeWork/L01_HomeWork/L01_HomeWork.sln
    	new file:   01/HomeWork/L01_HomeWork/L01_HomeWork/L01_HomeWork.csproj
    	new file:   01/HomeWork/L01_HomeWork/L01_HomeWork/Program.cs
    	new file:   ClassWork02/ClassWork02.sln
    	new file:   ClassWork02/ClassWork02/ClassWork02.csproj
    	new file:   ClassWork02/ClassWork02/Program.cs

---
+++ 01/HomeWork/L01_HomeWork/L01_HomeWork/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace DemoApplication
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine("Введите имя");
+			string name = Console.ReadLine();
+			Thread.Sleep(5000);
+			Console.WriteLine($"Hello, {name}!");
+			Thread.Sleep(5000);
+			Console.WriteLine($"Good Bye, {name}!");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ ClassWork02/ClassWork02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassWork02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); //hhhhhh
+            //gggggggg
+        }
+        
+    }
+}
