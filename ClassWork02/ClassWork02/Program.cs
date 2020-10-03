97f7aa77072eafe0426bec8d21a59ac90bf39547
Michael <michael.golovkov@gmail.com>
2020-10-03 14:17:06 +0300

    ClassWork 02 added

--- ClassWork02/ClassWork02/Program.cs
+++ ClassWork02/ClassWork02/Program.cs
@@ -6,8 +6,12 @@ namespace ClassWork02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!"); //hhhhhh
-            //gggggggg
+            Console.Write("Tell yor age, please: ");
+            string age = Console.ReadLine();
+            int personAge = int.Parse(age);
+            Console.Write("After 10 years your  age will be: ");
+            Console.WriteLine(personAge + 10);
+
         }
         
     }
