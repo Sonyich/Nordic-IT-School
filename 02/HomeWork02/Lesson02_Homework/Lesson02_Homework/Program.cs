94e95c58543825dca245d7adab67c9f317b1ec96
Sofya <sonicbor@yandex.ru>
2020-10-05 21:37:23 +0300

    Homework for second lesson added

---
+++ 02/HomeWork02/Lesson02_Homework/Lesson02_Homework/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sum of this numbers is: {0}", firstNumber + secondNumber);
+            Console.WriteLine("Difference of this numbers is: {0} ", firstNumber - secondNumber);
+            Console.WriteLine("Product of this numbers is: {0}", firstNumber * secondNumber);
+        }
+    }
+}
---
+++ 02/HomeWork02/Lesson02_additional_homework/Lesson02_additional_homework/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Lesson02_additional_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers and operation (+,-,*, /,% or **) : ");
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            string operation = Console.ReadLine();
+            double result = 0;
+
+            if (operation == "+") {
+                result = firstNumber + secondNumber;
+            }
+
+            if (operation == "-")
+            {
+                result = firstNumber - secondNumber;
+            }
+
+            if (operation == "*")
+            {
+                result = firstNumber * secondNumber;
+            }
+
+            if (operation == "/")
+            {
+                if (secondNumber == 0)
+                {
+                    Console.WriteLine("It is forbidden");
+                }
+                else
+                {
+                    result = Convert.ToSingle(firstNumber) / Convert.ToSingle(secondNumber);
+                }
+            }
+
+            if (operation == "%")
+            {
+                result = firstNumber % secondNumber;
+            }
+
+            if (operation == "**")
+            {
+                double firstNumberToDouble = Convert.ToDouble(firstNumber);
+                double secondNumberToDouble = Convert.ToDouble(secondNumber);
+
+                result = Math.Pow(firstNumberToDouble, secondNumberToDouble);
+            }
+
+            Console.WriteLine("Result is: {0}", result);
+        }
+    }
+}
