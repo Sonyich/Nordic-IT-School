d3a8b3a2cdb98c0200a002912002cddf347edee3
Sofya <sonicbor@yandex.ru>
2020-12-07 23:37:10 +0300

    12 added

---
+++ 12/ClassWork/Classwork12/Classwork12/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Classwork12
+{
+    public class BaseDocument
+    {
+        public string DocName
+        {
+            get;
+            set;
+        }
+
+        public string DocNumber
+        {
+            get; set;
+        }
+
+        public DateTimeOffset IssueDate
+        {
+            get; set;
+        }
+
+        virtual public string PropertiesString
+        {
+            get
+            {
+                return $"Docname:{DocName}\n" + $"DocNumber:{DocNumber}\n" + $"IssueDate:{IssueDate}";
+            }
+        }
+
+        virtual public void WriteToConsole()
+        {
+            Console.WriteLine(PropertiesString);
+        }
+        
+        public BaseDocument(string docName, string docNumber, DateTimeOffset issueDate)
+        {
+            DocName = docName;
+            DocNumber = docNumber;
+            IssueDate = issueDate;
+        }
+        
+    }
+
+    public class Passport : BaseDocument
+    {
+        public string Country { get; set; }
+        public string PersonName { get; set; }
+
+        override public string PropertiesString
+        {
+            get
+            {
+                return $"Docname:{DocName} \n" + $"DocNumber:{DocNumber}\n" + $"IssueDate:{IssueDate}\n" + $"Country:{Country}\n" +
+                    $"" + $"PersonName:{PersonName}";
+            }
+        }
+
+        override public void WriteToConsole()
+        {
+            Console.WriteLine(PropertiesString);
+        }
+        public Passport (string docName, string docNumber, DateTimeOffset issueDate):base("Passport", docNumber, issueDate)
+        {
+ 
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseDocument Visum = new BaseDocument("1", "2", DateTimeOffset.Parse("22/07/2018"));
+            /*
+            {
+                DocName = "1",
+                DocNumber = "2",
+                IssueDate = DateTimeOffset.Parse("
+            22/07/2018")
+            };
+            */
+            var Pass1 = new Passport("5", "6", DateTimeOffset.Parse("22/10/2018"));
+            /*
+            {
+                Pass1.DocName = "5",
+                Pass1.DocNumber = "6",
+                Pass1.IssueDate = DateTimeOffset.Parse("22/10/2018"),
+                Pass1.Country = "Russia",
+                Pass1.PersonName = "Oleg"
+            };
+            */
+
+
+            Visum.WriteToConsole();
+            Pass1.WriteToConsole();
+
+
+        }
+    }
+
+}
---
+++ 12/HomeWork/HomeWork12/HomeWork12/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace HomeWork11
+{
+    public partial class ReminderItem
+    {
+        public DateTimeOffset AlarmDate { get; set; }
+
+        public string AlarmMessage { get; set; }
+
+        public TimeSpan TimeToAlarm
+        {
+            get
+            {
+                return (DateTimeOffset.Now - AlarmDate);
+            }
+        }
+
+        public bool IsOutdated
+        {
+            get
+            {
+                if (TimeToAlarm < TimeSpan.Parse("0:00:00"))
+                    return false;
+                else
+                    return true;
+            }
+        }
+    }
+
+    public partial class ReminderItem
+    {
+        public ReminderItem (DateTimeOffset alarmDate, string alarmMessage)
+        {
+            AlarmDate = alarmDate;
+            AlarmMessage = alarmMessage;
+        }
+
+        virtual public void WriteProperties()
+        {
+            Console.WriteLine(
+                $"{GetType().Name}" + "\n" +
+                $"AlarmDate: {AlarmDate}" + "\n"
+                + $"AlarmMessage: {AlarmMessage}" + "\n"
+                + $"TimeToAlarm: {TimeToAlarm}" + "\n"
+                + $"IsOutdated: {IsOutdated}");
+        }
+    }
+
+    public class PhoneReminderItem : ReminderItem
+    {
+        public string PhoneNumber { get; set; }
+
+        public PhoneReminderItem (
+            DateTimeOffset alarmDate, 
+            string alarmMessage, 
+            string phoneNumber)
+            : base (alarmDate, alarmMessage)
+        {
+            PhoneNumber = phoneNumber;
+        }
+        override public void WriteProperties()
+        {
+            Console.WriteLine(
+                $"{GetType().Name}" + "\n" +
+                $"AlarmDate: {AlarmDate}" + "\n"
+                + $"AlarmMessage: {AlarmMessage}" + "\n"
+                + $"TimeToAlarm: {TimeToAlarm}" + "\n"
+                + $"IsOutdated: {IsOutdated}" + "\n" +
+                $"PhoneNumber: {PhoneNumber}"); 
+        }
+    }
+
+    public class ChatReminderItem : ReminderItem
+    {
+        public string ChatName { get; set; }
+        public string AccountName { get; set; }
+        public ChatReminderItem (
+            DateTimeOffset alarmDate, 
+            string alarmMessage, 
+            string chatName, 
+            string accountName)
+            : base (alarmDate, alarmMessage)
+        {
+            ChatName = chatName;
+            AccountName = accountName;
+        }
+        override public void WriteProperties()
+        {
+            Console.WriteLine(
+                $"{GetType().Name}" + "\n" +
+                $"AlarmDate: {AlarmDate}" + "\n"
+                + $"AlarmMessage: {AlarmMessage}" + "\n"
+                + $"TimeToAlarm: {TimeToAlarm}" + "\n"
+                + $"IsOutdated: {IsOutdated}" + "\n" +
+                $"ChatName: {ChatName}" + "\n" +
+                $"AccountName: {AccountName}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Reminders = new ReminderItem[3];
+
+            Reminders[0] = new ReminderItem(DateTimeOffset.Parse("7:00:00 AM"), "It is time to wake up!");
+            Reminders[1] = new PhoneReminderItem(DateTimeOffset.Parse("7:15:00 AM"), "It is time to WAKE UP!", "+79854270698");
+            Reminders[2] = new ChatReminderItem(DateTimeOffset.Parse("7:15:00 AM"), "It is time to WAKE UP!", "Wakers220", "Sonyich");
+                       
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Remind1: " + "\n");
+            Console.ResetColor();
+            Reminders[0].WriteProperties();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n" + "Remind2: " + "\n");
+            Console.ResetColor();
+            Reminders[1].WriteProperties();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n" + "Remind3: " + "\n");
+            Console.ResetColor();
+            Reminders[2].WriteProperties();
+
+        }
+    }
+}
